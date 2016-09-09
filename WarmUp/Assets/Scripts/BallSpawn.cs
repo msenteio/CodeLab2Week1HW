f2f01736b02aef89f7de3459e8842dd5c8665e8c
msenteio <mls866@nyu.edu>
2016-09-09 18:01:37 -0400

    got balls to spawn

---
+++ WarmUp/Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawn : MonoBehaviour {
+
+	public Sprite[] sprites;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("Spawn", 1, 1);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void Spawn(){
+		GameObject go = Instantiate(Resources.Load("Prefabs/Color")) as GameObject;
+
+		int num = GetComponent<GetColor>().GetSpriteColor();
+
+		go.GetComponent<SpriteRenderer>().sprite = sprites[num];
+	}
+
+}
---
+++ WarmUp/Assets/Scripts/GetColor.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetColor : MonoBehaviour {
+
+	public virtual int GetSpriteColor(){
+		return Random.Range(0, 3);
+	}
+}
